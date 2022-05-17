1813d72846480e4445b6829d94d1dc35e7cc34ad
keshavalt22 <keshavkhatri35@gmail.com>
2022-05-17 18:41:12 +0530

    assignment

---
+++ block-BNaacn/model/address.js
@@ -0,0 +1,13 @@
+let mongoose = require("mongoose");
+
+let Schema = mongoose.Schema;
+
+let addressSchema = new Schema({
+    village: String,
+    city: String,
+    state: String,
+    pin: Number,
+    user: Schema.Types.ObjectId
+}, {timestamps: true});
+
+module.exports = mongoose,module("Address", addressSchema)
