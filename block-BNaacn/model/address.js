517964e9ee0528978a0917b063d9822ce92ab412
keshavalt22 <keshavkhatri35@gmail.com>
2022-05-17 19:00:14 +0530

    assignment

--- block-BNaacn/model/address.js
+++ block-BNaacn/model/address.js
@@ -4,10 +4,10 @@ let Schema = mongoose.Schema;
 
 let addressSchema = new Schema({
     village: String,
-    city: String,
-    state: String,
+    city:{type: String, required: true},
+    state: {type: String, required: true},
     pin: Number,
     user: Schema.Types.ObjectId
 }, {timestamps: true});
 
-module.exports = mongoose,module("Address", addressSchema)
+module.exports = mongoose.model("Address", addressSchema)
